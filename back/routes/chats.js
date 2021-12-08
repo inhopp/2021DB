3cd9df314354dee3a7cb59c8501555cf1ddc15c3
Monac0 <ehddn7361@gmail.com>
2021-12-08 17:50:37 +0900

    chats.js 스키마 맞춰서 수정

--- back/routes/chats.js
+++ back/routes/chats.js
@@ -3,13 +3,19 @@ const router = express.Router();
 const { query } = require('../modules/db');
 const { verifyMiddleWare } = require('../modules/jwt');
 
+//자신과 채팅을 나눈 적 있는 다른 유저와의 채팅방마다(row)
+//{ 다른 유저의 id, 마지막 메시지, 마지막 메시지의 시간}(column)을 모아서 리턴하는 api
+//성공시, success: true, chatList
+//실패시, success: false, errorMessage
+// - 마지막 메시지는 자신이 보낸 것일수도, 상대가 보낸 것일수도 있음
+// - 누가 보낸 것인지는 리턴하지 않음, 필요하면 추가 가능
 router.get('/list', verifyMiddleWare, async (req, res, next) => {
   const { id } = req.decoded;
 
   if (id) {
-    const chatList = (await query(`SELECT @uid:=user_id from users where id = '${id}';
-      WITH b AS (SELECT if(from_id = @uid, to_id, from_id) AS id, message, created_at FROM chatdatas WHERE from_id = @uid OR to_id = @uid)
-      SELECT u.id, u.name, b.message, b.created_at FROM b, users u WHERE created_at IN (SELECT max(created_at) FROM b GROUP BY id) and u.user_id = b.id;`))[1];
+    const chatList = (await query(`SELECT @uid:=id from users where id = '${id}';
+      WITH b AS (SELECT if(from_id = @uid, to_id, from_id) AS id, text, date_time FROM message WHERE from_id = @uid OR to_id = @uid)
+      SELECT u.id, u.name, b.text, b.date_time FROM b, users u WHERE date_time IN (SELECT max(date_time) FROM b GROUP BY id) and u.id = b.id;`))[1];
     res.json({
       success: true,
       chatList
@@ -22,14 +28,18 @@ router.get('/list', verifyMiddleWare, async (req, res, next) => {
   }
 });
 
+//targetId와 나눈 채팅들의 { from_id, to_id, text, date_time }을 시간순으로 정렬해서 리턴하는 api
+//req.body에 targetId 필요
+//성공시, success: true, chatDatas
+//실패시, success: false, errorMessage
 router.get('/chatData/:targetId', verifyMiddleWare, async (req, res, next) => {
   const { id } = req.decoded;
   const { targetId }= req.params;
 
   if (id) {
-    const chatDatas = await query(`SELECT message, a.f_i as from_id, a.t_i as to_id, created_at 
-      FROM chatDatas, (SELECT f.user_id as f_ui, f.id as f_i, t.user_id as t_ui, t.id as t_i FROM users f, users t WHERE (f.id = '${id}' and t.id = '${targetId}') OR (t.id = '${id}' and f.id = '${targetId}'))
-      WHERE (from_id = a.f_ui and to_id = a.t_ui) ORDER BY created_at ASC;`);
+    const chatDatas = await query(`SELECT text, a.f_id as from_id, a.t_id as to_id, date_time 
+      FROM message, (SELECT f.id as f_id, t.id as t_id FROM users f, users t WHERE (f.id = '${id}' and t.id = '${targetId}') OR (t.id = '${id}' and f.id = '${targetId}')) a
+      WHERE (from_id = a.f_id and to_id = a.t_id) ORDER BY date_time ASC;`);
     res.json({
       success: true,
       chatDatas
@@ -42,6 +52,8 @@ router.get('/chatData/:targetId', verifyMiddleWare, async (req, res, next) => {
   }
 });
 
+//무슨 함수인지 모르겠음, front에서 사용되는 곳을 못찾음
+//채팅방을 만드는 함수같은데, chatrooms가 다른 곳에서 사용이 안됨
 router.post('/', verifyMiddleWare, async (req, res, next) => {
   const { id } = req.decoded;
   const { name, description } = req.body;
